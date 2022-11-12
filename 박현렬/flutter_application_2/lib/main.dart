215774b67a2ae35427cd8ac4ee81c78721fa5434
hanni66 <72500673+hanni66@users.noreply.github.com>
2022-11-12 12:31:44 +0900

    [박현렬] 4주차 스터디 (StatefulWidget)
    This reverts commit 07d37b4ca02e65737220253fb4c6d88bc592b7bb.

--- 박현렬/flutter_application_2/lib/main.dart
+++ 박현렬/flutter_application_2/lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(useMaterial3: true), home: statelessTest());
+    return MaterialApp(home: statelessTest());
   }
 }
